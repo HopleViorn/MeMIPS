c3c076b888e9933df1109cec23049e52c452b9cc
Metheus <lujunran@foxmail.com>
2022-03-31 17:18:34 +0800

    create

--- inst_rom.v
+++ inst_rom.v
@@ -1,16 +1,26 @@
 module inst_rom(
     input wire rst,
     input wire[31:0] pc,
-    output reg[31:0] inst
+    input wire[31:0] pc_4,
+    output reg[31:0] inst1,
+    output reg[31:0] inst2
 );
 
 reg[31:0] rom [0:199];
 
 always @(*) begin
     if(rst) begin
-        inst=32'b0;
+        inst1=32'b0;
     end else begin
-        inst=rom[pc];
+        inst1=rom[pc];
+    end
+end
+
+always @(*) begin
+    if(rst) begin
+        inst1=32'b0;
+    end else begin
+        inst1=rom[pc_4];
     end
 end
 

efb35ecaf7176d7df08894d3fbb9502833ca855e
Metheus <lununran@foxmail.com>
2022-05-12 11:36:49 +0800

    add controal fi

--- decode/decode.sv
+++ decode/decode.sv
@@ -5,7 +5,9 @@ module decode(
 
     //issue_queue
     output logic[2:0] issue_queue_push_number,
-    input logic[2:0] iq_size_left
+    input logic[2:0] iq_size_left,
+
+    output bool stall_from_decode
 );
 
 ISSUE_QUEUE_ELEMENT[3:0] tmp;
@@ -19,7 +21,22 @@ generate
         );
     end
 endgenerate
-
 assign issue_queue_element=tmp;
+
+logic[2:0] valid_inst;
+always_comb begin
+    valid_inst=decode_require[0].is_valid
+    +decode_require[1].is_valid
+    +decode_require[2].is_valid
+    +decode_require[3].is_valid;
+
+    if(valid_inst>iq_size_left) begin
+        stall_from_decode=`true;
+        issue_queue_push_number=0;
+    end else begin
+        stall_from_decode=`false;
+        issue_queue_push_number=valid_inst;44
+    end
+end
 
 endmodule

a0483c6b62d997b87d842322bcd1d2d331566d69
Metheus <lununran@foxmail.com>
2022-05-13 20:51:26 +0800

    add JAL

--- src/decode/decoder.sv
+++ src/decode/decoder.sv
@@ -81,6 +81,28 @@ always_comb begin
         is_o.write_reg_need=`false;
         is_o.write_reg_addr=5'b0;
         end
+        `op_JAL:begin//jal imm28
+        is_o.num1_need=`false;
+        is_o.num1=32'b0;
+        is_o.num1_addr=5'b0;
+        is_o.num2_need=`false;
+        is_o.num2=32'b0;
+        is_o.num2_addr=5'b0;
+        is_o.accept_mask=3'b111;
+        
+        is_o.exe_type=brunch;
+        is_o.alu_op=alu_nop;
+        is_o.brunch_type=j;
+        is_o.llu_op=llu_nop;
+        is_o.memory_addr_offset=32'b0;
+
+        is_o.mem_write_ena=`false;
+        is_o.mem_read_ena=`false;
+        is_o.mem_type=wrd;
+
+        is_o.write_reg_need=`true;
+        is_o.write_reg_addr=5'd31;
+        end
         `op_BNE:begin//bne rs,rt,offset
         is_o.num1_need=`true;
         is_o.num1=32'b0;
@@ -94,7 +116,7 @@ always_comb begin
         is_o.alu_op=alu_nop;
         is_o.brunch_type=b;
         is_o.llu_op=llu_neq;
-        
+
         is_o.memory_addr_offset=32'b0;
 
         is_o.mem_write_ena=`false;

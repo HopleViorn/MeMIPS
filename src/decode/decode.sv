19f672e8365e66d77a505d694f85f43d19d96421
Metheus <lununran@foxmail.com>
2022-05-13 17:07:13 +0800

    Add Brunch Prediction

--- src/decode/decode.sv
+++ src/decode/decode.sv
@@ -22,14 +22,8 @@ generate
     end
 endgenerate
 assign issue_queue_element=tmp;
-
-logic[2:0] valid_inst;
-always_comb begin
-    valid_inst=decode_require[0].is_valid
-    +decode_require[1].is_valid
-    +decode_require[2].is_valid
-    +decode_require[3].is_valid;
-
+wire[2:0] valid_inst=decode_require[0].valid_number;
+always_comb begin;
     if(valid_inst>iq_size_left) begin
         stall_from_decode=`true;
         issue_queue_push_number=0;
